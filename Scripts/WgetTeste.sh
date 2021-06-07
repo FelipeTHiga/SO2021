132365070c6823750f2b4df85edb9006391f7a06
FelipeTHiga <felipe.higa@bandtec.com.br>
2021-06-07 19:33:57 -0300

    Alteracao no Jar

--- Scripts/WgetTeste.sh
+++ Scripts/WgetTeste.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget 'https://github.com/FelipeTHiga/SO2021/blob/3f8cf32da58b9f2279e33e8a86332649bfcc9d3c/Fluid.jar' -P /home/${USER}/Desktop/Fluid 1> /dev/null 2> /dev/stdout
-
+wget 'https://github.com/FelipeTHiga/SO2021/blob/main/Spring%20Examples-jar-with-dependencies.jar' -P /home/${USER}/Desktop/Fluid 1> /dev/null 2> /dev/stdout
